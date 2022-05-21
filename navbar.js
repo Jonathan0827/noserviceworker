5c119d6f9e4bb34e27ad46908fc827b2dda04e25
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-05-21 16:23:37 +0900

    Navigation bar UI update

--- navbar.js
+++ navbar.js
@@ -1,10 +1,26 @@
 document.write(`<nav>
-<ul>
-    <li><div class="dot"></div></li>
-    <li><a href="index.html">Home</a></li>
-    <li>
-        <a href="https://www.github.com/Jonathan0827">My GitHub</a>
-    </li>
-</ul>
-</nav>`);
-//#19E8CF
+	<a href="/">
+		<img alt="Logo" src="icon.png" height="35" />
+	</a>
+	<ul>
+		<li>
+			<a href="#">About me</a>
+		</li>
+		<li>
+			<a href="https://github.com/Jonathan0827?tab=repositories">
+				Projects
+			</a>
+			<ul>
+				<li>
+                <a href="https://github.com/Jonathan0827/BMI-Calculator">
+                BMI Calculator for iOS
+                </a>
+                <a href="https://jonathan0827.github.io/webFeature">
+						Old version of this page
+					</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</nav>
+`);
