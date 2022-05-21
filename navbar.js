c6c39d5a64627fb30962a2d5f22b526e94a498d6
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-05-21 15:44:45 +0900

    Stability update

---
+++ navbar.js
@@ -0,0 +1,10 @@
+document.write(`<nav>
+<ul>
+    <li><div class="dot"></div></li>
+    <li><a href="index.html">Home</a></li>
+    <li>
+        <a href="https://www.github.com/Jonathan0827">My GitHub</a>
+    </li>
+</ul>
+</nav>`);
+//#19E8CF
