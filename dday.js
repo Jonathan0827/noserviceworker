a2c1abada0701f282d62fc03c5ea052658ae2f3a
Jun Hyeop <limjunehyeop@gmail.com>
2022-06-01 10:53:41 +0000

    D-day

---
+++ dday.js
@@ -0,0 +1,18 @@
+var dday = new Date("June 7, 2022, 00:02:00").getTime();
+
+setInterval(function () {
+	var today = new Date().getTime();
+	var gap = dday - today;
+	var d = Math.floor(gap / (1000 * 60 * 60 * 24));
+	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
+	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
+	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
+	if (gap <= 0) {
+		document.getElementById("count").innerHTML =
+			"iOS 16, iPadOS 16, macOS 13, watchOS 8, tvOS 16";
+	} else {
+		document.getElementById(
+			"count"
+		).innerHTML = `<h2>${d} days ${h} hours ${m} minutes ${s} seconds left untill`;
+	}
+}, 1);
