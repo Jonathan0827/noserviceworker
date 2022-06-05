915fa25d01232e6575f4c420de43667d439e8173
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-06-05 17:27:55 +0900

    Added main.js for global code, Making animation

---
+++ main.js
@@ -0,0 +1,111 @@
+document.getElementById("head").innerHTML = `<link
+rel="apple-touch-icon"
+sizes="57x57"
+href="/apple-icon-57x57.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="60x60"
+href="/apple-icon-60x60.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="72x72"
+href="/apple-icon-72x72.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="76x76"
+href="/apple-icon-76x76.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="114x114"
+href="/apple-icon-114x114.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="120x120"
+href="/apple-icon-120x120.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="144x144"
+href="/apple-icon-144x144.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="152x152"
+href="/apple-icon-152x152.png"
+/>
+<link
+rel="apple-touch-icon"
+sizes="180x180"
+href="/apple-icon-180x180.png"
+/>
+<link
+rel="icon"
+type="image/png"
+sizes="192x192"
+href="/android-icon-192x192.png"
+/>
+<link
+rel="icon"
+type="image/png"
+sizes="32x32"
+href="/favicon-32x32.png"
+/>
+<link
+rel="icon"
+type="image/png"
+sizes="96x96"
+href="/favicon-96x96.png"
+/>
+<link
+rel="icon"
+type="image/png"
+sizes="16x16"
+href="/favicon-16x16.png"
+/>
+<link rel="manifest" href="/manifest.json" />
+
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+<link rel="stylesheet" href="./style.css" />
+
+<meta charset="utf-8" />
+<meta name="description" content="My description" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+`; //head
+
+document.getElementById("nav").innerHTML = `<nav>
+<a href="index.html">
+    <img alt="Logo" src="icon.png" height="35" />
+</a>
+<ul>
+    <li>
+        <a href="dday.html">D-day</a>
+    </li> 
+    <li>
+        <a href="about.html">About me</a>
+    </li>
+    <li>
+        <a href="https://github.com/Jonathan0827?tab=repositories">
+            Projects
+        </a>
+        <ul>
+            <li>
+            <a href="https://github.com/Jonathan0827/BMI-Calculator">
+            BMI Calculator for iOS
+            </a>
+            <a href="https://github.com/JonathanBots/ChatBot">
+                    ChatBot for Discord
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="/#feedbackSection">Feedback</a>
+    </li>
+</ul>
+</nav>`;
