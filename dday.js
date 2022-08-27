e850bd40ee150db72ee3c721a37257e8e03cd807
Jun Hyeop <limjunehyeop@gmail.com>
2022-08-27 02:08:31 +0000

    Far Out

--- dday.js
+++ dday.js
@@ -1,8 +1,13 @@
-var dday = new Date("August 24, 2022, 09:00:00").getTime();
-
+var dday = new Date("September 8, 2022, 02:00:00").getTime();
 setInterval(function () {
-	var today = new Date().getTime();
-	var gap = dday - today;
+	var today = new Date();
+	const utc = 
+      today.getTime() + 
+      (today.getTimezoneOffset() * 60 * 1000);
+	  const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+const kr_curr = 
+      new Date(utc + (KR_TIME_DIFF));
+	var gap = dday - kr_curr;
 	var d = Math.floor(gap / (1000 * 60 * 60 * 24));
 	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
 	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
@@ -12,6 +17,6 @@ setInterval(function () {
 	} else {
 		document.getElementById(
 			"count"
-		).innerHTML = `<h2>${d} days ${h} hours ${m} minutes ${s} seconds left untill second semester.`;
+		).innerHTML = `<h2>${d} days ${h} hours ${m} minutes ${s} seconds left untill <a href="https://www.apple.com/apple-events/">Apple Far Out</a> event.`;
 	}
 }, 1);
