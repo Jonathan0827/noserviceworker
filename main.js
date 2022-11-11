40e0f62c0b60aa2119e5e648f1d9b15a5db5a946
Jun Hyeop <limjunehyeop@gmail.com>
2022-11-11 09:03:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -76,11 +76,8 @@ href="/favicon-16x16.png"
 
 <link rel="stylesheet" href="./style.css" />
 <link rel="stylesheet" href="aos.css"> 
-<script>
-  if (typeof navigator.serviceWorker !== 'undefined') {
-    navigator.serviceWorker.register('sw.js')
-  }
-</script>
+    
+
 <script src="https://redirectrussia.org/v1.js" data-redirect-url="https://jonathan.kro.kr" data-detection="ip-only" async integrity="sha384-K4/XEYup4kNv/qt2ucIwIH2wLT9I+z3s17CHQNMBB2/E8/Kw2VYsXQKB/7kylubA" crossorigin="anonymous"></script>
 
 <meta charset="utf-8" />
