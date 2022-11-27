fe569afc821f44cb13910243a29f93e4173f2cd3
Jun Hyeop <limjunehyeop@gmail.com>
2022-11-28 08:12:38 +0900

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -2,7 +2,7 @@ function subFeedback() {
 	const title = document.getElementById("title").value;
 	const message = document.getElementById("message").value;
 
-	const link = `https://github.com/Jonathan0827/jonathan0827.github.io/discussions/new?body=${message}&title=${title}`;
+	const link = `https://github.com/Jonathan0827/noserviceworker/discussions/new?body=${message}&title=${title}`;
 	window.open(link);
 	console.log(link);
 }
